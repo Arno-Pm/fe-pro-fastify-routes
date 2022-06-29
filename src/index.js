d617573ce2980fe70afed137be8a26192c28f461
Arno-Pm <70451630+Arno-Pm@users.noreply.github.com>
2022-06-29 23:16:37 +0300

    made hm

--- src/index.js
+++ src/index.js
@@ -11,4 +11,47 @@ fastify.register(import('@fastify/multipart'), {
 });
 fastify.register(import('@fastify/cookie'));
 
+fastify.post('/uppercase', (request, reply) => {
+  const str = request.body.toUpperCase();
+
+  if (str.includes('FUCK')) {
+    return reply.status(403).send('unresolved')
+  } else {
+    return reply.status(200).send(str);
+  }
+});
+
+fastify.post('/lowercase', (request, reply) => {
+  const str = request.body.toLowerCase();
+  if (str.includes('fuck')) {
+    return reply.status(403).send('unresolved')
+  } else {
+    return reply.status(200).send(str);
+  }
+});
+
+fastify.get('/user/:id', (request, reply)=>{
+  
+  let id = request.params.id;
+
+  if (users[id]) {
+    return reply.send(users[id]);
+  }
+  return reply.code(400).send('User not exist');
+});
+
+fastify.get('/users', function (request, reply) {
+  let usersArray = Object.values(users);
+  let { filter, value } = request.query;
+
+if (request.query) {
+    usersArray = usersArray.filter(function (user) {
+      return user[filter] == value;
+    })
+  }
+
+  console.log(usersArray);
+
+  return reply.send(usersArray);
+});
 export default fastify;
